cb84b37932770e03d10b5b72673aa2e0c1d043c9
alex-the-pigeon <xelakart@gmail.com>
2023-12-15 21:59:19 +1100

    feat: add navbar

--- client-test/src/App.jsx
+++ client-test/src/App.jsx
@@ -5,7 +5,49 @@ function App() {
   
   return (
     <div>
-      <header className='p-4'>
+      <header>
+      <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+        <li>
+          <a>Locations</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li>
+        <details>
+          <summary>Locations</summary>
+          <ul className="p-2">
+            <li><a>The Grand Lodge</a></li>
+            <li><a>The Wilkes Cabin</a></li>
+            <li><a>The Fairvale Motel</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Contact</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Sign In</a>
+  </div>
+</div>
+
+      {/* <header className='p-4'>
         <a href="" className="flex items-center gap-1">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
           <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
@@ -16,7 +58,7 @@ function App() {
         <div className='flex items-right gap-1'>Home</div>
         <div></div>
         <div></div>
-        <div></div>
+        <div></div> */}
       </header>
       </div>
       
